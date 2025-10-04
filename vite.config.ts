290284a34525e653d7cf600cd21a65b89db7c736
Mitesh <sanchaniyamitesh2610@gmail.com>
2025-10-04 16:49:45 +0530

    Github Deployment Config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/shipping-box-calculator/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
